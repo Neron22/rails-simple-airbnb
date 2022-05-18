1a5a59a7176139440912761e3bc2eaff945ecd3e
Valentin Renon <valentin.renon@gmail.com>
2022-05-18 16:46:15 +0200

    add index show new in flats controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,32 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+    # @reviews = @flats.reviews
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+    # @reviews = @flat.reviews
+    # @review = Review.new
+  end
+
+  def new
+    @flat = Flat.new
+    # @review = Review.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :address, :price_per_night, :number_of_guests)
+  end
 end
