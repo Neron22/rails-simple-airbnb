0fc23340d97e9d286d1cabb09ea72173acae5a13
Valentin Renon <valentin.renon@gmail.com>
2022-05-18 14:48:05 +0200

    create flat model flats controller and 4 flats in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'incredible flat',
+  address: '10 road incredible',
+  description: 'Amazing amazing amazingable bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 105,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Humid flat',
+  address: '10 underwater street',
+  description: 'live a unique humid experience',
+  price_per_night: 55,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Aerial bridge flat',
+  address: '10 under a famous bridge',
+  description: 'discover the street camping',
+  price_per_night: 15,
+  number_of_guests: 8
+)
